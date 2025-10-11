4dce53331ce9c0a1d4ddaaf36df84a593ef9b2e3
Otto Peräkylä <otto.perakyla@gmail.com>
2025-10-11 15:26:15 +0300

    move stuff around

--- src/matrix.js
+++ src/matrix.js
@@ -1,29 +1,3 @@
-function det2x2(A) {
-  let [[a, b], [c, d]] = A
-  return a*d - b*c
-}
-
-function det(A) {
-  if (A.length !== A[0].length) {
-    throw new Error(`Expected square matrix, but got dimensions [${A.length}, ${A[0].length}].`)
-  }
-
-  if (A.length === 2) {
-    return det2x2(A)
-  }
-
-  let [_, ...rows] = A
-  let sum = 0
-  
-  for (let i = 0; i < A.length; i++) {
-    let coefficient = A[0][i]
-    let submatrix = rows.map(row => row.filter((_,cellIdx) => cellIdx !== i))
-    sum += Math.pow(-1, i) * coefficient * det(submatrix)
-  }
-
-  return sum
-}
-
 function is_square(A) {
   return A.length === A[0].length
 }
@@ -43,6 +17,32 @@ function is_identity(A) {
   }
 
   return true
+}
+
+function det2x2(A) {
+  let [[a, b], [c, d]] = A
+  return a*d - b*c
+}
+
+function det(A) {
+  if (!is_square(A)) {
+    throw new Error(`Expected square matrix, but got dimensions [${A.length}, ${A[0].length}].`)
+  }
+
+  if (A.length === 2) {
+    return det2x2(A)
+  }
+
+  let [_, ...rows] = A
+  let sum = 0
+  
+  for (let i = 0; i < A.length; i++) {
+    let coefficient = A[0][i]
+    let submatrix = rows.map(row => row.filter((_,cellIdx) => cellIdx !== i))
+    sum += Math.pow(-1, i) * coefficient * det(submatrix)
+  }
+
+  return sum
 }
 
 module.exports = {
