e315b37d648811454dc1e7721a278ec7cfe0c752
Otto Peräkylä <otto.perakyla@gmail.com>
2025-10-12 10:27:46 +0300

    remove stuff

--- tests/test-matrix.js
+++ tests/test-matrix.js
@@ -18,6 +18,11 @@ test('simple 2x2 determinant', () => {
 test('3x3 determinant', () => {
     assert.strictEqual(det([[1,2,3],[4,5,6],[7,8,9]]), 0)
     assert.strictEqual(det([[1,5,3],[4,8,6],[7,8,9]]), -18)
+    assert.strictEqual(det([
+        [0,1,0],
+        [1,0,0],
+        [0,0,1]
+    ]), -1)
 });
 
 test('4x4 determinant', () => {
